5842d47a77d8f533c37c6ea12af3f5bde85ae66c
simonetussi <simoetussi2006@gmail.com>
2026-08-13 19:19:49 +0200

    asdf

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //    ]
   //  }
   //
+  
   const SVG_ZOOM = `<svg viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none">
     <circle cx="11" cy="11" r="8"/>
     <line x1="21" y1="21" x2="16.65" y2="16.65"/>
